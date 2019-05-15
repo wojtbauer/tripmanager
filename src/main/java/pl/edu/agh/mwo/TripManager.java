42df261a5a78c9b07182cef326d05837d7cf6d23
wojtbauer <wojciech.brk1@gmail.com>
2019-05-15 14:59:37 +0200

    TripManager class implementation

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -25,4 +25,26 @@ public class TripManager {
 		tripList.remove(name);
 	}
 	
+	public List<Trip> findTrip(String keyword) {
+		List<Trip> results = new ArrayList<Trip>();
+        for (String tripName : tripList.keySet()) {
+            if (keyword.isEmpty()) {
+				results.add(tripList.get(tripName));
+            }
+        	if (tripName.toLowerCase().contains(keyword.toLowerCase()) ||
+                    tripList.get(tripName).getDescription().toLowerCase().contains(keyword.toLowerCase())) {
+            	results.add(tripList.get(tripName));
+                return results;
+            }
+        	List<Photo> photos = tripList.get(tripName).getPhotos();
+        	for (Photo photo : photos) {
+        		if (photo.getComment().contains(keyword)) {
+					results.add(tripList.get(tripName));
+				}
+			}
+        }
+        return results;
+		
+	}
+	
 }
